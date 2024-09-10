22a44234d2087ddf1a50a733c6d2174773f0aa05
Ronisha Shreshta <137037810+ronishastha495@users.noreply.github.com>
2024-09-10 11:14:36 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId  = 144534
+let accountEmail = "ronisha@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jhapa "
+let accountState
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// not to use var because of issue in block scope and functional scope
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3  ) we are using nodejs, not browser
+
+console.log(3+3) // code readability should be high
+
+let name = "ronisha"
+let age = 24
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+ 
